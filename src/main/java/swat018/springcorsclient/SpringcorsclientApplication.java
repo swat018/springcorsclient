85517124aab9eb0315f7f51db6bc1b77ce5a2e56
jinwoo park <swat018@icloud.com>
2021-08-05 13:14:29 +0900

    Initial commit

---
+++ src/main/java/swat018/springcorsclient/SpringcorsclientApplication.java
@@ -0,0 +1,14 @@
+package swat018.springcorsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SpringcorsclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcorsclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/swat018/springcorsclient/SpringcorsclientApplicationTests.java
@@ -0,0 +1,13 @@
+package swat018.springcorsclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcorsclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
